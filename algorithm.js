c143ac57bdc47ec0f6201eef9c0ddb1aade70cc3
davidmshou <david.m.shou@gmail.com>
2015-03-02 13:38:56 -0800

    Attempts sorting.

--- algorithm.js
+++ algorithm.js
@@ -38,13 +38,114 @@ var shuffle = function(m) {
 
 var deck = new Deck();
 
+/*var sort = function() {
+  $("li:eq(0)").insertAfter($("li:eq(2)"));
+}
+sort();*/
+
+// var length = $(".card").length;
+
+/*for (i = 0, i < $(".card").length, i++) {
+  $("li:eq(i)").insertBefore($("li:eq(i+1)"));
+}*/
+
+/*function bubblesort(arr){
+  var i, swapped;
+ 
+  do{
+    for (i = 1; i <= arr.length; i++){
+      for (j =1; j <= arr.length; j++){
+        swapped = false;
+        while (arr[j-1] > arr[j]){
+          var temp = arr[j];
+          console.log(temp);
+          arr[j] = arr[j-1];
+          arr[j-1] = temp;
+          swapped = true;
+          }
+        }
+      }
+    } while (swapped);
+  return arr;
+}*/
+
+/*$('#deshuffle').on('click', function() {
+  for (var i = 0, j; i < ($(".card").length); i++) {
+
+   var temp = $(".card[i]");
+
+   for (j =i-1; j>=0 && $(".card[j]") > temp; j--){
+     $(".card[j+1]") = $(".card[j]");
+
+     $('li:eq(' + j + ')').
+     before($('li:eq(' + i + ')')).
+     after($('li:eq(' + i + ')'));
+
+   }
+   $(".card[j+1]") = temp;
+ }
+}*/
+
+
+
+
+/*var mylist = $('.card');
+var listitems = mylist.children('div').get();
+listitems.sort(function(a, b) {
+   var compA = $(a).text();
+   var compB = $(b).text();
+   return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
+})
+$.each(listitems, function(idx, itm) { mylist.append(itm); });
+});*/
+
+
+
+/*var sort = function(){
+ for (var i = 0, j; i < array.length; i++) {
+
+   var temp = array[i];
+
+   for (j =i-1; j>=0 && array[j] > temp; j--){
+     array[j+1] = array[j];
+
+     $('li:eq(' + j + ')').
+     before($('li:eq(' + i + ')')).
+     after($('li:eq(' + i + ')'));
+
+   }
+   array[j+1] = temp;
+ }
+};*/
+
+
 
 $("#shuffle").on("click", function() {
   shuffle($(".cards").length);
   $("#deck").addClass("moveCards");
 });
 
+
 $("#deshuffle").on("click", function() {
-  location.reload(); 
+  for (var i = 0, j; i < ($(".card").length); i++) {
+
+   var temp = ($(".card[i]"));
+
+   for (j =i-1; j>=0 && ($(".card.get[j]")) > temp; j--){
+     ($(".card.get[j+1]")) = ($(".card.get[j]"));
+
+     ($('li:eq(' + j + ')')).
+     before($('li:eq(' + i + ')')).
+     after($('li:eq(' + i + ')'));
+
+   }
+   ($(".card.get[j+1]")) = temp;
+ }
 });
+
+
+
+/*$("#deshuffle").on("click", function() {
+  location.reload(); 
+});*/
 
